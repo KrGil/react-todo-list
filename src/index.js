dd473c13e56b4f006cdc3da0c7ec442c5d6a0742
eisen <kanjjang89@naver.com>
2023-04-28 11:28:20 +0900

    fix: TRACKING_ID initialize

--- src/index.js
+++ src/index.js
@@ -5,6 +5,8 @@ import reportWebVitals from './reportWebVitals';
 import ReactGA from 'react-ga';
 
 const TRACKING_ID = process.env.REACT_APP_GOOGLE_ANALYTICS_TRACKING_ID;
+ReactGA.initialize(TRACKING_ID);
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
